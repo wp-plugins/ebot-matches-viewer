c1db9063f430bd4779c3dd6519aec765096acd70
ndjbouman <ndjbouman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-04 16:36:34 +0000

    '1.1'
    git-svn-id: https://plugins.svn.wordpress.org/ebot-matches-viewer/tags/1.0@851330 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/ebot-matches-viewer.php
@@ -0,0 +1,195 @@
+<!-- Ici Le CSS du tableau d'affichage -->
+<style>
+table.ebot tr:nth-child(odd){
+  background-color:#282828 ;
+}
+table.ebot {
+	border: 0pt;
+}
+th {
+	border: 1pt solid black;
+	-moz-border-radius:10px;
+	-webkit-border-radius:10px;
+	border-radius:10px;
+}
+tr.border_bottom td {
+	border-bottom: 1pt solid black;
+	border-right: 0pt;
+}
+</style>
+<!-- FIN du CSS -->
+<?php
+/*
+Plugin Name: eBot Matches Viewer
+Plugin URI: https://github.com/Asso-nOStReSs/eBot-matches-viewer
+Description: Un simple widget pour intégrer les matchs de l'eBot sur votre site communautaire.
+Author: Boudjelal Yannick *Bouman*
+Version: 1.0
+Author URI: www.boudjelalyannick.fr
+*/
+
+add_action('widgets_init','emv_init');
+
+function emv_init(){
+	register_widget("emv_widget");
+}
+
+class emv_widget extends WP_widget{
+	
+	function emv_widget() {
+		$options = array(
+			"classname"=>"ebot-matches",
+			"description"=>"Affiche les scrores, effectuer avec l'eBot sur vos serveurs."
+		);
+	/*	$control = array(
+			"width"=>1000,
+			"height"=>500
+		);
+	*/
+		$this->WP_widget("emv-ebot-matches","eBoT Matches Viewer",$options);
+	}
+	function widget($args,$d){
+		extract($args);
+		echo $before_widget;
+		echo $before_title.$d["titre"].$after_title;
+		?>
+		<?
+			if ($d["typeconnect"] == "A"){
+				/* Connection Distante mysql */
+				$host= $d["host"];
+				$dbnamedist= $d["dbnamedistant"];
+				$userdist= $d["userdistant"];
+				$passworddist= $d["passworddistant"];
+				$nbrmax= $d["nbrmax"];
+					try{
+					$bdd = new PDO('mysql:host='.$host.';dbname='.$dbnamedist.'', ''.$userdist.'', ''.$passworddist.'');
+					$affreq = $bdd->prepare('SELECT * FROM matchs ORDER BY id DESC LIMIT 0, '.$nbrmax.'');
+					$affreq->execute();
+					}
+					catch (Exception $e)
+					{
+							die('Erreur : ' . $e->getMessage());
+					}			
+			}
+			if ($d["typeconnect"] == "B"){
+				echo "Option B en developement";
+			}
+			if ($d["typeconnect"] == "C"){
+				$dbnamelocal= $d["dbnamelocal"];
+				$userlocal= $d["userlocal"];
+				$passwordlocal= $d["passwordlocal"];
+				$nbrmax= $d["nbrmax"];
+					try{
+					$bdd = new PDO('mysql:host=localhost;dbname='.$dbnamelocal.'', ''.$userlocal.'', ''.$passwordlocal.'');	
+					$req = 'SELECT * FROM matchs ORDER BY id DESC LIMIT 0, '.$nbrmax.'';
+					
+					echo 'Connecter à la base Mysql locale. <br />';	
+					}
+					catch (Exception $e)
+					{
+							die('Erreur : ' . $e->getMessage());
+					}	
+			}			
+					echo'<table class="ebot">';
+					echo "<tr><th>#Id</th><th>Score</th></tr>";
+					while($row = $affreq->fetch(PDO::FETCH_ASSOC)){
+						$team1name= $row['team_a_name'];
+						$team2name= $row['team_b_name'];
+						$team1scr= $row['score_a'];
+						$team2src= $row['score_b'];
+
+						echo "<tr class='border_bottom'><td>";
+						echo $row['id'];
+						echo "</td><td>";
+							if($team1scr>$team2src) 
+								echo '<strong>'.$team1name.'&nbsp;-&nbsp;<font color="green">'.$team1scr.'</strong></font>&nbsp;:&nbsp;<font color="red">'.$team2src.'</font>&nbsp;-&nbsp;'.$team2name.'';
+							elseif($team1scr<$team2src) 
+								echo ''.$team1name.'&nbsp;-&nbsp;<font color="red">'.$team1scr.'</font>&nbsp;:&nbsp;<font color="green">'.$team2src.'</font>&nbsp;-&nbsp;<strong>'.$team2name.'</strong>';
+							else 
+								echo ''.$team1name.'&nbsp;-&nbsp;<font color="bleue">'.$team1scr.'</font>&nbsp;:&nbsp;<font color="bleue">'.$team2src.'</font>&nbsp;-&nbsp;'.$team2name.'';
+						echo "</td></tr>";
+					}
+					echo'</table>';
+		echo $after_widget;
+		$bdd == null;
+	}
+	function update($new,$old){
+		return $new;
+	}
+	function form($d) {
+		$defaut = array(
+			"titre" => "eBoT Matches",
+			"nbrmax" => "5",
+			"userdistant" => "ebotv3",
+			"userlocal" => "ebotv3",
+			"dbnamedistant" => "ebotv3",
+			"dbnamelocal" => "ebotv3"
+		);
+		$d = wp_parse_args($d,$defaut)
+		?>
+			<div id="form">
+			<p>
+				<label for="<?php echo $this->get_field_id("titre"); ?>">Titre : </label>
+				<input value="<?echo $d["titre"];?>" name="<?php echo $this->get_field_name("titre"); ?>" id="<?php echo $this->get_field_id("titre"); ?>" type="text"/>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id("nbrmax"); ?>">Nombre max de match : </label>
+				<input value="<?echo $d["nbrmax"];?>" name="<?php echo $this->get_field_name("nbrmax"); ?>" id="<?php echo $this->get_field_id("nbrmax"); ?>" type="text" maxlength="1"/>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id("typeconnect"); ?>">Type de connection : </label>
+				<select value="<?echo $d["typeconnect"];?>" name="<?php echo $this->get_field_name("typeconnect");?>" id="<?php echo $this->get_field_id("typeconnect"); ?>">
+					<option value="A">Distant (Option:A)</option>
+					<option value="B">Online (Option:B)</option>
+					<option value="C">Local (Option:C)</option>
+				</select>
+			</p>
+			</div>
+			<div id="A" class="divoption">
+				<p>Option A "Distant":</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("host"); ?>">Ip du Host : </label>
+						<input value="<?echo $d["host"];?>" name="<?php echo $this->get_field_name("host"); ?>" id="<?php echo $this->get_field_id("host"); ?>" type="text"/>
+					</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("dbnamedistant"); ?>">Nom de la Base de donnée : </label>
+						<input value="<?echo $d["dbnamedistant"];?>" name="<?php echo $this->get_field_name("dbnamedistant"); ?>" id="<?php echo $this->get_field_id("dbnamedistant"); ?>" type="text"/>
+					</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("userdistant"); ?>">Utilisateur "login" : </label>
+						<input value="<?echo $d["userdistant"];?>" name="<?php echo $this->get_field_name("userdistant"); ?>" id="<?php echo $this->get_field_id("userdistant"); ?>" type="text"/>
+					</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("passworddistant"); ?>">Password : </label>
+						<input value="<?echo $d["passworddistant"];?>" name="<?php echo $this->get_field_name("passworddistant"); ?>" id="<?php echo $this->get_field_id("passworddistant"); ?>" type="text"/>
+					</p>
+			</div>
+			<hr>
+			<div id="B" class="divoption">
+				<p>Option B "Online": "Non fonctionnel"</p>
+					<p> En developement !
+						<!-- <label for="<?php echo $this->get_field_id("nom-team"); ?>">Nom Team à afficher : </label>
+						<input value="<?echo $d["nom-team"];?>" name="<?php echo $this->get_field_name("nom-team"); ?>" id="<?php echo $this->get_field_id("nom-team"); ?>" type="text"/> -->
+					</p>
+			</div>
+			<hr>
+			<div id="C" class="divoption">
+				<p>Option C "Local:</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("dbnamelocal"); ?>">Nom de la Base de donnée : </label>
+						<input value="<?echo $d["dbnamelocal"];?>" name="<?php echo $this->get_field_name("dbnamelocal"); ?>" id="<?php echo $this->get_field_id("dbnamelocal"); ?>" type="text"/>
+					</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("userlocal"); ?>">Utilisateur "login" : </label>
+						<input value="<?echo $d["userlocal"];?>" name="<?php echo $this->get_field_name("userlocal"); ?>" id="<?php echo $this->get_field_id("userlocal"); ?>" type="text"/>
+					</p>
+					<p>
+						<label for="<?php echo $this->get_field_id("passwordlocal"); ?>">Password : </label>
+						<input value="<?echo $d["passwordlocal"];?>" name="<?php echo $this->get_field_name("passwordlocal"); ?>" id="<?php echo $this->get_field_id("passwordlocal"); ?>" type="text"/>
+					</p>
+			</div>
+			<p>Merci DeStrO pour l'eBOt. Widget dev. par Bouman.</p>
+		<?
+	}
+}	
+?>
---
+++ trunk/readme.txt
@@ -0,0 +1,96 @@
+=== eBot Matches Viewer ===
+Contributors: Boudjelal Yannick *Bouman*
+Tags: eBot, matche, matches, match, matchs, 
+Requires at least: 3.0
+Tested up to: 3.8.1
+Stable tag: 3.8.1
+License: GPLv2 or later
+License URI: https://github.com/Asso-nOStReSs/eBot-matches-viewer
+
+Un simple widget pour intégrer les matchs de l'eBot sur votre site.
+
+== Description ==
+Salut à tous,
+Grâce à ce plugin d'affichage de résultats de matchs Counter-Strike Global Offensive (CSGO), vous n'avez plus besoin d'ajouter manuellement vos matchs CSGO dans vos panel d'admin de votre site web.
+Vous avez juste à faire vos matchs sur vos serveur CSGO avec l'eBot et ce plugin récupère et affiche vos matchs sur votre site communautaire ou multi-gaming.
+Simple et efficace.
+////////////////////////
+Hi everybody,
+With this plugin display results of Counter-Strike Global Offensive (CSGO) Matches, you no longer need to manually add your matches CSGO in the admin panel of your website. 
+You just have to make your games on your server with the CSGO EbOt and this plugin retrieves and displays your games on your community site or multi-gaming.
+Simple and effective.
+
+== Installation ==
+
+1. Upload the `ebot-matches-viewer` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure options
+
+Options "A" :  Your server Mysql eBot is different that website.
+
+    IP-ebot           		 IP-web
+     ebot                  serveur web
+     ____                     ____
+    |    |    Connection     |    |
+    |    | <---------------> |    |
+    |____|  mysql distant    |____|
+	
+	Step 1: Give power in phpmyadmin on Server eBot:
+			SQL Command : 
+				grant all privileges on *.* to user@IP-web identified by "destress";   ===> //// user = (Exemple: ebotv3)  IP-web = (Exemple: gw8.ovh.net) is your website is from OVH.
+				flush privileges;
+				
+	Step 2: Connecting on dedicated server by SSH - Putty - :
+			Command :
+				nano /etc/mysql/my.cnf
+			And edit :	
+				Remplace : bind-address = 127.0.0.1
+				to : #bind-address = 127.0.0.1
+				
+				//comments french:
+				 Par défaut, MySQL n'écoute que localhost. . Il faudra désactiver la ligne (ajout de : '#') relative au bind-address dans le fichier de configuration mysql:
+				# bind-address = 127.0.0.1
+			Restart :
+				/etc/init.d/mysql restart
+
+				
+Ready You can configure Widget for connect on database Distant.
+			
+Options "B" :  eBot online (http://ebot.esport-tools.net/)       -------------------------------------->        En developpement pour avoir une connection vers la base sql de eBoT online 
+
+ ebot.esport-tools.net     	 IP-web
+     ebot                  serveur web
+     ____                     ____
+    |    |    Connection     |    |
+    |    | <---------------> |    |
+    |____|  mysql distant    |____|
+
+
+Options "C" :  Your server Mysql eBot and website are the same.
+
+ IP-ebot = IP-web
+ serveur web + ebot
+     ________
+    |        |
+    |        |
+    |________|
+				
+Ready You can configure Widget for connect on database local with user and password  -------> (Exemple : user=ebotv3 password=ebotv3).
+
+4.Configure Number of matches to display
+
+== Frequently Asked Questions ==
+
+Write at : ndjbouman@gmail.com
+
+== Screenshots ==
+1. View Client,
+2. View Options
+
+== Changelog ==
+= 1.5 = Preview
+* Mode Option "B" eBot Online working.
+
+= 1.0 =
+Display last number matches
+* Initial Plugin Launch
